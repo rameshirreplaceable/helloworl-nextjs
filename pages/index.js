b24109d90fa7bcbd28bcd1f154a776fe072ca2c3
Ramesh M <41829618+rameshirreplaceable@users.noreply.github.com>
2021-05-26 16:01:09 +0530

    tetst test

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
-import Link from 'next/link'
+import Link from "next/link";
 
 export default function IndexPage() {
   return (
     <div>
-      Hello World.{' '}
+      Hello World.{" "}
       <Link href="/about">
-        <a>About</a>
+        <a>About 123</a>
       </Link>
     </div>
-  )
+  );
 }
